708a948f848dd0080b3937ba4b5b0f952bfc4915
Jiacheng-Lyu <52687126+Jiacheng-Lyu@users.noreply.github.com>
2023-12-17 20:17:12 +0800

    Add files via upload

--- hiv_model_web.py
+++ hiv_model_web.py
@@ -9,7 +9,7 @@ import joblib
 
 import numpy as np
 
-input_feature_type = st.sidebar.radio("Navigation Menu",["Home", "Clinical features","Proteome features","Combined clinical and proteome features"])
+input_feature_type = st.sidebar.radio("Navigation Menu",["Home", "Clinical features","Combined clinical and proteome features"])
 
 #Home Page 
 if input_feature_type == "Home":
@@ -17,19 +17,14 @@ if input_feature_type == "Home":
     # st.image("Medical Prediction Home Page.jpg")
     st.text("The Following Predicted Features Are Available ->")
     st.text("1. Clinical features")
-    st.text("2. Proteome features")
-    st.text("3. Combined clinical and proteome features")
+    st.text("2. Combined clinical and proteome features")
     st.text("")
     st.text("The Following Predicted Follow-up Windows Are Available ->")
-    st.text("1. 3 to 6 months")
-    st.text("2. 6 to 9 months")
-    st.text("3. 9 to 12 months")
-    st.text("3. More than 12 months")
+    st.text("1. Less than 9 months")
+    st.text("2. More than 9 months")
 
-
-
-input_type_name_spaces = {'Clinical features': 'clinical', 'Proteome features': 'proteome', 'Combined clinical and proteome features': 'all'}
-prediction_window_name_spaces = {'3to6': '3 to 6 months', '6to9': '6 to 9 months', '9to12': '9 to 12 months', 'more-than-12': 'More than 12 months'}
+input_type_name_spaces = {'Clinical features': 'clinical', 'Combined clinical and proteome features': 'all'}
+prediction_window_name_spaces = {'less-than-9': 'Less than 9 months', 'more-than-9': 'More than 9 months'}
 respond_state_name_spaces = {1: 'Non Respond', 0: 'Respond'}
 
 def return_predict_result(predict_out):
@@ -48,8 +43,9 @@ def predict_state(input_feature_type, input_value):
 
 models = defaultdict(dict)
 for f in os.listdir('./models'):
-    name1, name2, *_ = f.split('_')
-    models[name1][name2] = joblib.load(os.path.join('./models', f))
+    if f.endswith('joblib'):
+        name1, name2, *_ = f.split('_')
+        models[name1][name2] = joblib.load(os.path.join('./models', f))
 
 
 if input_feature_type == 'Clinical features':
@@ -62,24 +58,7 @@ if input_feature_type == 'Clinical features':
     cd4_B = st.number_input("The count of CD4+ cells when you first accept the cART")
     treatment_year = age - age_first_cART
 
-    input_value = np.array([[cd4_L, age, cd4_B, treatment_year]])
-    predict_out = predict_state(input_feature_type, input_value)
-
-    if st.button("Predict"):
-        return_predict_result(predict_out)
-
-if input_feature_type == 'Proteome features':
-    st.header("Predict whether you will respond to cART in the future")
-    st.write("Providing two decimal place will make the prediction more accurate")
-
-    apol1 = st.number_input("The APOL1 protein expression level")
-    acaca = st.number_input("The ACACA protein expression level")
-    serpind1 = st.number_input("The SERPIND1 protein expression level")
-    myh14 = st.number_input("The MYH14 protein expression level")
-    pkm = st.number_input("The PKM protein expression level")
-    syne1 = st.number_input("The SYNE1 protein expression level")
-
-    input_value = np.array([[apol1, acaca, serpind1, myh14, pkm, syne1]])
+    input_value = np.array([[cd4_B, cd4_L, age, treatment_year]])
     predict_out = predict_state(input_feature_type, input_value)
 
     if st.button("Predict"):
@@ -97,13 +76,10 @@ if input_feature_type == 'Combined clinical and proteome features':
     treatment_year = age - age_first_cART
 
     apol1 = st.number_input("The APOL1 protein expression level")
-    acaca = st.number_input("The ACACA protein expression level")
-    serpind1 = st.number_input("The SERPIND1 protein expression level")
-    myh14 = st.number_input("The MYH14 protein expression level")
-    pkm = st.number_input("The PKM protein expression level")
-    syne1 = st.number_input("The SYNE1 protein expression level")
+    ppbp = st.number_input("The PPBP protein expression level")
+    acat2 = st.number_input("The ACAT2 protein expression level")
 
-    input_value = np.array([[apol1, acaca, serpind1, cd4_L, age, cd4_B, myh14, pkm, syne1, treatment_year]])
+    input_value = np.array([[cd4_B, cd4_L, apol1, ppbp, acat2, age, treatment_year]])
     predict_out = predict_state(input_feature_type, input_value)
 
     if st.button("Predict"):
